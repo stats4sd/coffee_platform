48f2016e4e6a8240e1ea21caaa9877e6dcbe00a4
Dave Mills <d.e.mills@stats4sd.org>
2021-03-01 17:43:48 +0000

    adds SelectWithImages component element

---
+++ resources/js/components/elements/SelectWithImages.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="d-flex flex-wrap justify-content-center">
+        <image-option
+            v-for="option in options"
+            :key="option.id"
+        >
+            <b-card
+                no-body
+                class="select-card dark-gradient m-2"
+                border-variant="primary"
+                :class="option.id === value ? 'selected' : ''"
+                @click="$emit('input', option.id)"
+            >
+                <b-card-img
+                    :src="option.image"
+                />
+                <div class="card-caption text-white font-weight-bold">
+                    {{ option.label }}
+                </div>
+            </b-card>
+        </image-option>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            'multiple': {
+                type: Boolean,
+                default: false,
+            },
+            'options': {
+                type: Array,
+                default: () => [],
+            },
+            'value': {
+                type: Number,
+                default: null,
+            },
+        },
+    }
+</script>
