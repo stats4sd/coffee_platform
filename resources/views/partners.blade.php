6152e3e277d95cdf49112edab7aab9d85fb83c53
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2021-04-01 14:47:21 +0100

    fix title Partners

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
 <div class="container mt-5">
-    <h1>SSCF Partners</h1>
+    <h1>Partners</h1>
     <p>
         This project brings together the Agroecology and Livelihoods Collaborative (ALC) at the University of Vermont (UVM), Statistics for Sustainable Development (Stats4SD), Lutheran World Relief (LWR), and Heifer International.
     </p>
