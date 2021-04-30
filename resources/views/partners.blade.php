cc8532f4083be70fb68438d5c07c732a0ffe373d
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2021-04-30 16:42:00 +0100

    add partners logos

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -6,5 +6,17 @@
     <p>
         This project brings together the Agroecology and Livelihoods Collaborative (ALC) at the University of Vermont (UVM), Statistics for Sustainable Development (Stats4SD), Lutheran World Relief (LWR), and Heifer International.
     </p>
+    <div id="app">
+    <template>
+        <div>
+            <b-row>
+                <b-col><b-img  height="100" src="storage/partners/UVM.jpg"  alt="UVM"></b-img></b-col>
+                <b-col class="mt-5"><b-img  height="45" src="storage/partners/Stats4SD_Logo_Red_Small.jpg" alt="Stats4SD"></b-img></b-col>
+                <b-col><b-img  height="100" src="storage/partners/LWR.png" alt="LWR"></b-img></b-col>
+                <b-col><b-img  height="100" src="storage/partners/Heifer.jpg" alt="Heifer"></b-img></b-col>
+            </b-row>
+        </div>
+        </template>
+    </div>
 </div>
 @endsection
