d9530a8f2bdb33228823806cd404303c6664292f
Dave Mills <d.e.mills@stats4sd.org>
2021-05-14 16:35:28 +0100

    updates year crud

--- app/Http/Controllers/Admin/YearCrudController.php
+++ app/Http/Controllers/Admin/YearCrudController.php
@@ -21,7 +21,7 @@ class YearCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,25 +33,25 @@ class YearCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
         // CRUD::setFromDb(); // columns
-        CRUD::addColumn(['name' => 'year', 'type' => 'year']); 
+        CRUD::addColumn(['name' => 'year', 'type' => 'year']);
 
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -59,18 +59,12 @@ class YearCrudController extends CrudController
     {
         CRUD::setValidation(YearRequest::class);
 
-        CRUD::setFromDb(); // fields
-
-        /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
-         */
+        CRUD::field('year')->type('number');
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
