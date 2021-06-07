dc520d619ff05bdc94a1d7c1ec9749c7488bcb4d
Alex Thomson <alex.thomson@stats4sd.org>
2021-06-07 17:51:34 +0100

    Update table and graph functions

---
+++ scripts/Rscript/Function dev.R
@@ -0,0 +1,232 @@
+library(tidyverse)
+library(openxlsx)
+library(kableExtra)
+library(knitr)
+library(scales)
+library(flextable)
+
+data1 <- read.xlsx("example data/Example data 3.xlsx")
+
+countries <- unique(data1$country)
+
+indicator_codes <- unique(data1$code)
+
+data1 <- data1%>%
+  group_by(country, year, source_name, purpose, gender, scope)%>%
+  mutate(n = n())%>%
+  mutate(group = ifelse(n > 1, row_number(), NA))%>%
+  mutate(indicator_definition = "-")%>%
+  ungroup()
+
+data1%>%
+  filter(country == countries[1])%>%
+  select(country,purpose, gender, scope, source_name, value_standard, year, group)%>%
+  arrange(year)%>%
+  mutate(Purpose = paste0(purpose, "\n",
+                          "Gender: ", gender, "\n",
+                          "Scope: ", scope))%>%
+  mutate(source_name = ifelse(!is.na(group), paste0(source_name, " Group:", group), source_name))%>%
+  select(-purpose, -scope, -gender, -group)%>%
+  pivot_wider(names_from = year, values_from = value_standard)%>%
+  mutate_all(as.character)%>%
+  rename("Source" = "source_name")%>%
+  replace(is.na(.), "-")%>%
+  select(-country)%>%
+  mutate(Purpose = as.factor(Purpose))%>%
+  arrange(Purpose)%>%
+  as_grouped_data("Purpose")%>%
+  as_flextable()%>%
+  add_header_lines(values = countries[1])%>%
+  bold(i = ~!is.na(Purpose))%>%
+  bg(i = 1, bg = "#A8D08D", part = "header")%>%
+  bg(i = 2, bg = "#EDEDED", part = "header")%>%
+  bg(i = ~!is.na(Purpose), bg = "#E2EFD9")%>%
+  theme_box()%>%
+  width(j = 1, width = 3)
+
+data1%>%
+  filter(country == countries[2])%>%
+  select(country,purpose, gender, scope, source_name, value_standard, year, group)%>%
+  arrange(year)%>%
+  mutate(Purpose = paste0(purpose, "\n",
+                          "Gender: ", gender, "\n",
+                          "Scope: ", scope))%>%
+  mutate(source_name = ifelse(!is.na(group), paste0(source_name, " Group:", group), source_name))%>%
+  select(-purpose, -scope, -gender, -group)%>%
+  pivot_wider(names_from = year, values_from = value_standard)%>%
+  mutate_all(as.character)%>%
+  rename("Source" = "source_name")%>%
+  replace(is.na(.), "-")%>%
+  select(-country)%>%
+  mutate(Purpose = as.factor(Purpose))%>%
+  arrange(Purpose)%>%
+  as_grouped_data("Purpose")%>%
+  as_flextable()%>%
+  add_header_lines(values = countries[2])%>%
+  bold(i = ~!is.na(Purpose))%>%
+  bg(i = 1, bg = "#A8D08D", part = "header")%>%
+  bg(i = 2, bg = "#EDEDED", part = "header")%>%
+  bg(i = ~!is.na(Purpose), bg = "#E2EFD9")%>%
+  theme_box()%>%
+  width(j = 1, width = 3)
+
+data1%>%
+  filter(country == countries[3])%>%
+  select(country,purpose, gender, scope, source_name, value_standard, year, group)%>%
+  arrange(year)%>%
+  mutate(Purpose = paste0(purpose, "\n",
+                          "Gender: ", gender, "\n",
+                          "Scope: ", scope))%>%
+  mutate(source_name = ifelse(!is.na(group), paste0(source_name, " Group:", group), source_name))%>%
+  select(-purpose, -scope, -gender, -group)%>%
+  pivot_wider(names_from = year, values_from = value_standard)%>%
+  mutate_all(as.character)%>%
+  rename("Source" = "source_name")%>%
+  replace(is.na(.), "-")%>%
+  select(-country)%>%
+  mutate(Purpose = as.factor(Purpose))%>%
+  arrange(Purpose)%>%
+  as_grouped_data("Purpose")%>%
+  as_flextable()%>%
+  add_header_lines(values = countries[3])%>%
+  bold(i = ~!is.na(Purpose))%>%
+  bg(i = 1, bg = "#A8D08D", part = "header")%>%
+  bg(i = 2, bg = "#EDEDED", part = "header")%>%
+  bg(i = ~!is.na(Purpose), bg = "#E2EFD9")%>%
+  theme_box()%>%
+  width(j = 1, width = 3)
+
+
+value_table <- function(data, x, y){
+  value_table <- data%>%
+    filter(code == x & country == y)%>%
+    select(country,purpose, gender, scope, source_name, value_standard, year, group)%>%
+    arrange(year)%>%
+    mutate(Purpose = paste0(purpose, "\n",
+                            "Gender: ", gender, "\n",
+                            "Scope: ", scope))%>%
+    mutate(source_name = ifelse(!is.na(group), paste0(source_name, " Group:", group), source_name))%>%
+    select(-purpose, -scope, -gender, -group)%>%
+    pivot_wider(names_from = year, values_from = value_standard)%>%
+    mutate_all(as.character)%>%
+    rename("Source" = "source_name")%>%
+    replace(is.na(.), "-")%>%
+    select(-country)%>%
+    mutate(Purpose = as.factor(Purpose))%>%
+    arrange(Purpose)%>%
+    as_grouped_data("Purpose")%>%
+    as_flextable()%>%
+    add_header_lines(values = y)%>%
+    bold(i = ~!is.na(Purpose))%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    bg(i = 2, bg = "#EDEDED", part = "header")%>%
+    bg(i = ~!is.na(Purpose), bg = "#E2EFD9")%>%
+    theme_box()%>%
+    width(j = 1, width = 3)
+  
+  return(value_table)
+  
+}
+
+value_table(data1, indicator_codes[1], countries[1])
+
+
+bar_graph <- function(data, x){
+  
+  data <- data%>%
+    filter(code == x)  
+  
+  p1 <- data%>%
+    ggplot(aes(fill = country, y=value_standard, x = year))+
+    geom_col(position = position_dodge2(preserve = "single"))+
+    labs(fill = "Country",
+         x = "Year",
+         y = paste0(data$name, " (", data$unit_standard[1], ")"),
+         title = paste(data$code, " ", data$name))+
+    scale_fill_brewer(palette = "Dark2")+
+    theme_light()
+  
+  if(data$value_standard[1] == "%"){
+    
+    p1 <- scale_y_continuous(labels = percent)
+    
+  }else{}
+  
+  return(p1)
+  
+}
+
+bar_graph(data1, indicator_codes[1])
+
+
+#definition table
+t <-data1%>%
+  select(source_name, source_partner, indicator_name_original, indicator_definition)%>%
+  group_by(source_name)%>%
+  slice(1)
+  
+t <- flextable(t)%>%
+  set_header_labels(source_name = "Source",
+                    source_partner = "Source Partner",
+                    indicator_name_original = "Original Indicator Name",
+                    indicator_definition = "Indicator Definition")%>%
+  bg(i = 1, bg = "#A8D08D", part = "header")%>%
+  theme_box()%>%
+  width(j = 1:4, width = c(2,1,1,2))
+
+t
+
+definition_table <- function(data, x){
+  t <-data%>%
+    filter(code == x)%>%
+    select(source_name, source_partner, indicator_name_original, indicator_definition)%>%
+    group_by(source_name)%>%
+    slice(1)
+  
+  definition_table <- flextable(t)%>%
+    set_header_labels(source_name = "Source",
+                      source_partner = "Source Partner",
+                      indicator_name_original = "Original Indicator Name",
+                      indicator_definition = "Indicator Definition")%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    theme_box()%>%
+    width(j = 1:4, width = c(2,1,1,2))
+  
+  return(definition_table)
+  
+}
+
+definition_table(data1, indicator_codes[1])
+
+report_characteristics <- function(data){
+  report_characteristics <- data.frame(
+    Detail = c(
+      rep("Country", length(unique(data1$country))),
+      rep("Years", length(unique(data1$year))),
+      rep("Source Type", length(unique(data1$source_partner_type))),
+      rep("Purpose", length(unique(data1$purpose))),
+      rep("Gender", length(unique(data1$gender))),
+      rep("Scope", length(unique(data1$scope)))),
+    Value = c(
+      unique(data1$country),
+      unique(data1$year),
+      unique(data1$source_partner_type),
+      unique(data1$purpose),
+      unique(data1$gender),
+      unique(data1$scope)
+    )
+  )
+  
+  report_characteristics <- flextable(report_characteristics)%>%
+    set_header_labels(Detail = "Report Characteristics",
+                      Value = "Report Characteristics")%>%
+    merge_v(j = 1)%>%
+    merge_h(part = "header")%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    theme_box()%>%
+    autofit()
+  
+  return(report_characteristics)
+}
+
+report_characteristics(data1)
--- scripts/Rscript/functions.R
+++ scripts/Rscript/functions.R
@@ -4,70 +4,130 @@ library(kableExtra)
 library(knitr)
 library(scales)
 
-details_table <- function(data, x){
-  data <- data%>%
-    filter(`Indicator code` == x)
+report_characteristics <- function(data, x){
+  report_characteristics <- data.frame(
+    Detail = c(
+      rep("Country", length(unique(data1$country))),
+      rep("Years", length(unique(data1$year))),
+      rep("Source Type", length(unique(data1$source_partner_type))),
+      rep("Purpose", length(unique(data1$purpose))),
+      rep("Gender", length(unique(data1$gender))),
+      rep("Scope", length(unique(data1$scope)))),
+    Value = c(
+      unique(data1$country),
+      unique(data1$year),
+      unique(data1$source_partner_type),
+      unique(data1$purpose),
+      unique(data1$gender),
+      unique(data1$scope)
+    )
+  )
   
-  details_table <- tibble(
-    Detail = c("Indicator", "Definition", "Indicator unit", "Internal indicator code",
-               "Member of"))
-  details_table$Comment[1] <- data$Indicator[1]
-  details_table$Comment[2] <- data$`Indicator definition`[1]
-  details_table$Comment[3] <- data$Unit[1]
-  details_table$Comment[4] <- data$`Indicator code`[1]
-  details_table$Comment[5] <- data$`Indicator Group`[1]
+  report_characteristics <- flextable(report_characteristics)%>%
+    set_header_labels(Detail = "Report Characteristics",
+                      Value = "Report Characteristics")%>%
+    merge_v(j = 1)%>%
+    merge_h(part = "header")%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    theme_box()%>%
+    autofit()
   
-  details_country <- tibble(Detail = rep("Countries included", length(unique(data$Country))),
-                            Comment = sort(unique(data$Country)))
-  
-  details_year <- tibble(Detail = rep("Years included", length(unique(data$Year))),
-                         Comment = sort(unique(data$Year)))
-  
-  details_table <- rbind(details_table, details_country)%>%
-    rbind(details_year)
-  
-  return(details_table)
+  return(report_characteristics)
 }
 
-indicator_table <- function(data, x){
-  data <- data%>%
-    filter(`Indicator code` == x)
+indicator_details <- function(data, x){
+  data <- data%>%filter(code == x)
   
-  Indicator_table <- data%>%
-    select(Country, Source, Year, Value)%>%
-    arrange(Year, Country)
+  value_table <- data.frame(
+    "col1" = c("Indicator", "Indicator Code", "Indicator unit"),
+    "col2" = c(data$name[1], x, data$unit_standard[1])
+  )
   
-  Indicator_table <- Indicator_table%>%
-    pivot_wider(id_cols = Country:Source, names_from = Year,
-                values_from = Value)
+  value_table <- flextable(value_table)%>%
+    set_header_labels(col1 = "", col2 = "")%>%
+    delete_part("header")%>%
+    border_remove()%>%
+    autofit()
   
-  Indicator_table <- Indicator_table %>%
-    mutate(across(everything(), as.character))%>%
-    arrange(Country)
+  return(value_table)
   
-  Indicator_table[is.na(Indicator_table)] <- ""
+}
+
+indicator_details(data1, indicator_codes[1])
+
+value_table <- function(data, x, y){
+  value_table <- data%>%
+    filter(code == x & country == y)%>%
+    select(country,purpose, gender, scope, source_name, value_standard, year, group)%>%
+    arrange(year)%>%
+    mutate(Purpose = paste0(purpose, "\n",
+                            "Gender: ", gender, "\n",
+                            "Scope: ", scope))%>%
+    mutate(source_name = ifelse(!is.na(group), paste0(source_name, " Group:", group), source_name))%>%
+    select(-purpose, -scope, -gender, -group)%>%
+    pivot_wider(names_from = year, values_from = value_standard)%>%
+    mutate_all(as.character)%>%
+    rename("Source" = "source_name")%>%
+    replace(is.na(.), "-")%>%
+    select(-country)%>%
+    mutate(Purpose = as.factor(Purpose))%>%
+    arrange(Purpose)%>%
+    as_grouped_data("Purpose")%>%
+    as_flextable()%>%
+    add_header_lines(values = y)%>%
+    bold(i = ~!is.na(Purpose))%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    bg(i = 2, bg = "#EDEDED", part = "header")%>%
+    bg(i = ~!is.na(Purpose), bg = "#E2EFD9")%>%
+    theme_box()%>%
+    width(j = 1, width = 3)
   
-  return(Indicator_table)
+  return(value_table)
   
 }
 
 
 bar_graph <- function(data, x){
-
-  data <- data%>%
-    filter(`Indicator code` == x)  
   
- p1 <- ggplot(data,
-       aes(fill = Country, y = Value/100, x = reorder(Year,desc(Year))))+
-  geom_col(position = position_dodge(preserve = "single"))+
-  scale_fill_brewer(palette = "Dark2")+
-  coord_flip()+
-  labs(x = "Year",
-       y = paste(data$Indicator.name[1]))+
-  scale_y_continuous(labels = percent)
- 
- return(p1)
+  data <- data%>%
+    filter(code == x)  
+  
+  p1 <- data%>%
+    ggplot(aes(fill = country, y=value_standard, x = year))+
+    geom_col(position = position_dodge2(preserve = "single"))+
+    labs(fill = "Country",
+         x = "Year",
+         y = paste0(data$name, " (", data$unit_standard[1], ")"),
+         title = paste(data$code, " ", data$name))+
+    scale_fill_brewer(palette = "Dark2")+
+    theme_light()
+  
+  if(data$value_standard[1] == "%"){
+    
+    p1 <- scale_y_continuous(labels = percent)
+    
+  }else{}
+  
+  return(p1)
   
 }
 
+definition_table <- function(data, x){
+  t <-data%>%
+    filter(code == x)%>%
+    select(source_name, source_partner, indicator_name_original, indicator_definition)%>%
+    group_by(source_name)%>%
+    slice(1)
   
+  definition_table <- flextable(t)%>%
+    set_header_labels(source_name = "Source",
+                      source_partner = "Source Partner",
+                      indicator_name_original = "Original Indicator Name",
+                      indicator_definition = "Indicator Definition")%>%
+    bg(i = 1, bg = "#A8D08D", part = "header")%>%
+    theme_box()%>%
+    width(j = 1:4, width = c(2,1,1,2))
+  
+  return(definition_table)
+  
+}  
