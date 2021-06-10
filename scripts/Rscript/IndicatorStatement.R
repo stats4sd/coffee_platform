bd34860146454221c963b164bec77e19575dcfb3
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2021-06-10 15:29:32 +0100

    test excel_read

--- scripts/Rscript/IndicatorStatement.R
+++ scripts/Rscript/IndicatorStatement.R
@@ -6,8 +6,6 @@ library(readxl)
 args <- commandArgs(TRUE)
 
 excelFile <- args[1]
-print('excelFile')
-print(excelFile)
 excelData <- read_excel(excelFile)
 
 #indicator_value_ids <- strsplit(args[2], ',')
