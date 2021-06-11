6a8723b84b81fba97c771f407babb0f357e77266
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2021-06-11 11:59:13 +0100

    updates front end design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,60 @@
 @extends('layouts.app')
 
 @section('main')
-<div class="container mt-5">
-    <p>
-        The State of the Coffee Smallholder Platform is an open access data resource for smallholder coffee farmers and different actors working with coffee smallholders who are using data for decision-making. The platform results from an extensive review, analysis and synthesis of existing data and information about coffee smallholders in Nicaragua, Guatemala, and Honduras. We have described the data collection process and key findings in detail in the <a href={{ url('/reports') }} style="color:blue;">report</a> .
-    </p>
-    <p>
-        The platform and the accompanied report complement other initiatives that – in various ways - seek to gather, synthesize, and analyze social, economic, and/or environmental data about coffee smallholders. While many initiatives focus on tracking and documenting coffee industry actors’ sustainability efforts, our approach is from the bottom up. Based on the available data, we are taking steps toward drawing a more holistic picture about coffee smallholders, their livelihoods, and the challenges they are facing. We are also seeking to identify data gaps, and vision how we could improve data collection and sharing among a variety of actors along the coffee value chain, including the farmers and their organizations.
-    </p>
-    <p>
-        This pilot project brings together the Agroecology and Livelihoods Collaborative (ALC) at the University of Vermont (UVM), Statistics for Sustainable Development (Stats4SD), Lutheran World Relief (LWR), and Heifer International. Several other organizations have contributed ideas and data for this project.
-    </p>
-</div>
+        
+    <div class="header">
+        <img src="images/home.jpg" id="image_home_header"/>
+        <div class ="text-block">
+            <h1>Coffee Smallholder Data</h1>
+            <p>Open access data for decision-making</p>
+            <p>Explore and download indicator data about smallholder coffee farmers in Guatemala, Honduras and
+                Nicaragua.
+            </p>
+            <div class="d-flex justify-content-center mt-5 mb-5">
+                <a href="indicators"><button class="button-green">Browse Indicators <i class='las la-arrow-right'></i></button></a>
+            </div>
+        </div>
+        <div id="caption">
+            Photo Credit: Phillip Davis 2020
+        </div>
+    </div>
+
+
+    <div class="container px-4">
+        <div class="row">
+            <h2 id="text-green">What is it?</h2>
+            <p>The State of the Coffee Smallholder Platform is an open access data resource, designed to drive more informed decision making
+                 amongst smallholder coffee farmers as well as various value chain actors.
+            </p>
+            <p>This platform collects together indicator data from numerous soures, which can be used to draw a
+                more holistic picutre about coffee smallholders, their livlihoods, and the challenges they are facing.
+                These are available to browse and download from the <a href="indicators" style="color: #66AC47">Indicator Hub.</a>
+            </p>
+            <div class="d-flex justify-content-center mt-5 mb-5">
+            <a href="partners"><button class="button-green">Find Out More <i class='las la-arrow-right'></i></button></a>
+            </div>             
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <h2 id="text-green">Report</h2>
+                <p>Alongside the platform, a report has been produced, describing the data collection process and
+                    the key findings in detail.
+                </p>
+                <p>In this report, read about the effort to identify data gaps and how data collection could be improved and
+                    shared among a variety of actors along the coffee value chain, including the farmers and their organizations.
+                </p>
+                <div class="d-flex justify-content-center mt-5 mb-5">
+                    <a href="reports"><button class="button-green">View the Report <i class='las la-arrow-right'></i></button></a>
+                </div>
+            </div>
+            <div class="col-6">
+                <img src="images/home_report.jpg" id="image_home_report"/>
+                <div id="caption_small">
+                    Photo Credit: Phillip Davis 2020
+                </div>
+            </div>            
+        </div>
+    </div>
+
+
 @endsection
--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -1,22 +1,49 @@
 @extends('layouts.app')
 
 @section('main')
-<div class="container mt-5">
-    <h1>Partners</h1>
+<div class="header">
+    <div> 
+        <img src="images/about.jpg" id="image_about"/>
+        <div id="caption">
+            Photo Credit: Phillip Davis 2019
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <h2 style="color:black">About</h2>
+    <p>The State of the Coffee Smallholder Platform is an open access data resource, designed to drive more informed decision making
+        amongst smallholder coffee farmers as well as various value chain actors.
+    </p>
+    <p> The resulting interactive site is based on an extensive review, analysis and synthesis of existing data and information about coffee smallholders
+        in Guatemala, Honduras, and Nicaragua.
+    </p>
+    <p> The platform and the accompanied <a href="reports" style="color: #66AC47">report</a> complement other initiatives that – in various ways - seek to gather, synthesize, and analyze social, economic, and/or environmental data about coffee smallholders. While many initiatives focus on tracking and documenting coffee industry actors’ sustainability efforts, our approach is from the bottom up. Based on the available data, we are taking steps toward drawing a more holistic picture about coffee smallholders, their livelihoods, and the challenges they are facing. We are also seeking to identify data gaps, and vision how we could improve data collection and sharing among a variety of actors along the coffee value chain, including the farmers and their organizations.
+
+     <h2 id="text-green">Partners</h2>
     <p>
-        This project brings together the Agroecology and Livelihoods Collaborative (ALC) at the University of Vermont (UVM), Statistics for Sustainable Development (Stats4SD), Lutheran World Relief (LWR), and Heifer International.
+        This project brings together Heifer International, Lutheran World Relief (LWR), Statistics for Sustainable Development (Stats4SD), and the Agroecology and Livelihoods Collaborative (ALC) at the University of Vermont (UVM).
     </p>
     <div id="app">
     <template>
         <div>
-            <b-row>
+            <!-- <b-row>
                 <b-col><b-img  height="100" src="images/partners/UVM.jpg"  alt="UVM"></b-img></b-col>
                 <b-col class="mt-5"><b-img  height="45" src="images/partners/Stats4SD_Logo_Red_Small.jpg" alt="Stats4SD"></b-img></b-col>
                 <b-col><b-img  height="100" src="images/partners/LWR.png" alt="LWR"></b-img></b-col>
                 <b-col><b-img  height="100" src="images/partners/Heifer.jpg" alt="Heifer"></b-img></b-col>
+            </b-row> -->
+            <b-row>
+            <b-col><b-img  height="100" src="images/partners/Heifer.jpg" alt="Heifer"></b-img></b-col>
+            <b-col><b-img  height="100" src="images/partners/LWR.png" alt="LWR"></b-img></b-col>
+            </b-row>
+            <b-row>
+            <b-col class="mt-5"><b-img  height="45" src="images/partners/Stats4SD_Logo_Red_Small.jpg" alt="Stats4SD"></b-img></b-col>
+            <b-col><b-img  height="100" src="images/partners/UVM.jpg"  alt="UVM"></b-img></b-col>
             </b-row>
         </div>
         </template>
     </div>
 </div>
+
 @endsection
