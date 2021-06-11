90e7bdf54bb582cbf1a3be59cb9c93c1a447c30b
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2021-06-11 12:51:58 +0100

    completed the frontend for the report page

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -3,36 +3,38 @@
 @section('main')
 
 <div class="header">
-        <img src="images/report.jpg" id="image_report"/>
-        <div id="caption">
-            Photo Credit: Phillip Davis 2020
-        </div>
-        <div class="container px-4">
-            <div class="row">
-                <div class="col-lg-6">
-                    <h2 style="color:black">Reports</h2>
-                    <p>Alongside the platform, the project has produced a report, describing the data collection
-                        process and key findings in detail.
-                    </p>
-                    <p>In this report, you can also read more about our efforts to identify data gaps and envision how we could imporve
-                        data collection and sharing among a variety of actors along the coffee value chain, including the farmers and
-                        their organizatins.
-                    </p>
-                </div>
-                <div>
-                    <button href="partners" class="button-green" id="about_button">Download 2021 Report
-                    <!-- <i class="fa fa-long-arrow-right" style="font-size:24px"></i></a> -->
-                    </button>
-                </div>
-            </div>    
-        </div>
+    <img src="images/report.jpg" id="image_report"/>
+    
+    
+    <div class="box-green">
+        <div class="container report-text ">
+            
+            <h2 style="color:black">Reports</h2>
+            <p>Alongside the platform, the project has produced a report, describing the data collection
+                process and key findings in detail.
+            </p>
+            <p>In this report, you can also read more about our efforts to identify data gaps and envision how we could imporve
+                data collection and sharing among a variety of actors along the coffee value chain, including the farmers and
+                their organizatins.
+            </p>
+        </div>    
+        
+    </div>
+    <div class="d-flex justify-content-center">
+        <a href="partners"><button class="button-green" id="report_button">Download 2021 Report <i class='las la-arrow-down'></i></button></a>
+    </div>
+    <div id="caption">
+        Photo Credit: Phillip Davis 2020
     </div>
 </div>
+            
+</div>
+        
+
 
 <div class="container px-4">
             <h2 id="text-green">Previous Reports</h2>
             <p>Here we could add donwload links to older reports...</p>
-        </div>
 </div>
 
 <!-- <div class="container-fluid justify-content-center">
