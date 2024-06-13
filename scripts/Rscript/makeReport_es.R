95eb33dc5b0e6120e2d751fc283d02385401de8b
Dave Mills <d.e.mills@stats4sd.org>
2024-06-13 12:14:37 +0100

    fix report download failure

--- scripts/Rscript/makeReport_es.R
+++ scripts/Rscript/makeReport_es.R
@@ -1,8 +1,12 @@
-## Load environment + setup paths to RMarkdown dependancies
+library(stringr)
+
+## Load environment + setup paths to RMarkdown dependencies
 # Required to ensure R called from CLI has the needed PATH entries to generate the pdf with PDF LATEX
 dotenv::load_dot_env("../../.env")
 Sys.setenv(PATH=paste0(
         Sys.getenv('PDFLATEX_PATH'),
+        ':',
+        Sys.getenv('RSTUDIO_PANDOC') %>% str_replace('pandoc', ''),
         ':',
         Sys.getenv('PATH')
     )
